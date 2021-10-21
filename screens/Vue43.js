da1f355eb3c19574795e08ded5d5508c8a9e37ba
Alexandre <77535743+Alex91700@users.noreply.github.com>
2021-10-21 14:37:18 +0200

    Style vues 4, 4x

--- screens/Vue43.js
+++ screens/Vue43.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { Button, StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 import { ThemeContext } from '../ThemeContext';
+import Contact from '../components/Contact'
 
 class Vue43 extends React.Component {
   
   render(){
     return (
       <View style={styles.container}>
-         <ImageBackground source={require('../assets/background@2x.png')} style={styles.backgroundStyle} >
-        <Text>Tourteau linguine</Text>
+        <ImageBackground source={require('../assets/background@2x.png')} style={styles.backgroundStyle} >
+          <Text style={styles.textStyle}>Tourteau linguine</Text>
+          <Image source={require('../assets/tourteauLinguine.png')} style={styles.imageStyle}/>
+          <Contact/>
         </ImageBackground>
       </View>
     );
@@ -32,5 +35,16 @@ const styles = StyleSheet.create({
     flex: 1
   }, scrollContainer: {
     flex: 1,
+  },
+  imageStyle: {
+    alignSelf: "center",
+    marginTop: "5%",
+    width: '50%',
+    height: '40%'
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: '180%',
+    color: 'white'
   },
 });
